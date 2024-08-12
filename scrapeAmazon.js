d3c0136d220b68f6e14c0c23f3268ed7eea3ec5e
thiagobruch <75326528+thiagobruch@users.noreply.github.com>
2024-08-11 22:24:50 -0400

    Update scrapeAmazon.js

--- scrapeAmazon.js
+++ scrapeAmazon.js
@@ -21,6 +21,9 @@ const totp = new OTPAuth.TOTP({
 // Generate OTP
 const token = totp.generate();
 
+//console.log(totp);
+//console.log(token);
+
 async function getOTP(secret) {
     const totp = new OTPAuth.TOTP({
         issuer: 'Amazon',
@@ -35,25 +38,44 @@ async function getOTP(secret) {
 
 (async () => {
     const browser = await puppeteer.launch({
-//({ headless: false });
-	    headless: true,
-	    defaultViewport: null,
-	    executablePath: '/usr/bin/google-chrome',
-	    args: ['--no-sandbox'],
-	  });
-    const page = await browser.newPage();
+            headless: true,
+            defaultViewport: null,
+	    userDataDir: './tmp',
+            args: ['--no-sandbox', '--disable-setuid-sandbox', '--single-process'],
+            produt: 'firefox',
+//            executablePath: '/usr/bin/google-chrome',
+          });
 
+    const page = await browser.newPage();
+	page.setDefaultTimeout(60000); // 60 seconds
+	
     // Navigate to Amazon login page
     await page.goto('https://www.amazon.com/ap/signin?openid.pape.max_auth_age=3600&openid.return_to=https%3A%2F%2Fwww.amazon.com%2Falexaquantum%2Fsp%2FalexaShoppingList%3Fref_%3Dlist_d_wl_ys_list_1&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=amzn_alexa_quantum_us&openid.mode=checkid_setup&language=en_US&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0');
-
+    sleep(1500, function() {
+    // delay
+    });
+    await page.goto('https://www.amazon.com/ap/signin?openid.pape.max_auth_age=3600&openid.return_to=https%3A%2F%2Fwww.amazon.com%2Falexaquantum%2Fsp%2FalexaShoppingList%3Fref_%3Dlist_d_wl_ys_list_1&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=amzn_alexa_quantum_us&openid.mode=checkid_setup&language=en_US&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0');
+    //await page.goto(amz_signin_url);	
     // Enter username
-    await page.type('#ap_email', amz_login);
 
-    // Enter password
-    await page.type('#ap_password', amz_password);
-    await page.click('#signInSubmit');
-    await page.waitForNavigation();
-
+	if (await page.$('#ap_password')) {
+//          console.log('Element #ap_password found!');
+            await page.type('#ap_email', amz_login);
+            await page.type('#ap_password', amz_password);
+            await page.click('#signInSubmit');
+            await page.waitForNavigation();
+	} else {
+//          console.log('Element #ap_password not found. Retrying...');
+            await new Promise(resolve => setTimeout(resolve, 1000)); // 30 second delay
+            await page.type('#ap_email', amz_login);
+            await page.click('#continue');
+            await page.waitForNavigation();
+//          const ids = await page.$$eval('[id]', elements => elements.map(el => el.id));
+//	    console.log('IDs found on the page:', ids);
+            await page.type('#ap_password', amz_password);
+            await page.click('#signInSubmit');
+            await page.waitForNavigation();
+	}
 
     // Handle OTP (if required)
     if (await page.$('#auth-mfa-otpcode')) {
@@ -63,7 +85,8 @@ async function getOTP(secret) {
     }
 
     // Navigate to Alexa Shopping List page
-    await page.goto('https://www.amazon.com/alexaquantum/sp/alexaShoppingList?ref_=list_d_wl_ys_list_1');
+    await page.goto('https://www.amazon.com/alexaquantum/sp/alexaShoppingList?ref_=list_d_wl_ys_list_1', { waitUntil: 'load', timeout: 60000 });
+    //await page.goto(amz_shoppinglist_url, { waitUntil: 'load', timeout: 60000 });
 
     const pageContent = await page.content();
     sleep(3000, function() {
@@ -90,7 +113,7 @@ async function getOTP(secret) {
 	
 
   // Display the JSON formatted list
-  console.log(jsonFormattedItems);
+  //console.log(jsonFormattedItems);
 
   // Close the browser when done
     await browser.close();
