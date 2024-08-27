591aabd18e0b43fe2b2dc6b845d88e386fa934c4
thiagobruch <75326528+thiagobruch@users.noreply.github.com>
2024-08-27 12:38:49 -0400

    Update script.sh

--- rootfs/app/script.sh
+++ rootfs/app/script.sh
@@ -2,6 +2,18 @@
 # Define the commands to run
 interval=$pooling_interval
 
+if [ "$log_level" == "true" ]; then
+        apk add mini_httpd
+        mkdir /app/www
+        echo port=8888  >> /etc/mini_httpd/mini_httpd.conf
+        echo user=minihttpd   >> /etc/mini_httpd/mini_httpd.conf
+        echo dir=/app/www  >> /etc/mini_httpd/mini_httpd.conf
+        echo nochroot  >> /etc/mini_httpd/mini_httpd.conf
+        apk add openrc  --no-cache
+        mkdir -p /run/openrc/exclusive
+        touch /run/openrc/softlevel
+        rc-service mini_httpd start
+
 COMMANDS=(
     "cd /app/"
     "rm -rf tmp/"
